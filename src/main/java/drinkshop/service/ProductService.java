43dc0f7817f0fcbd9c44fede2a71d9128513643e
vladmutu <vladmutu05@gmail.com>
2026-03-14 20:40:14 +0200

    .

--- src/main/java/drinkshop/service/ProductService.java
+++ src/main/java/drinkshop/service/ProductService.java
@@ -28,13 +28,6 @@ public class ProductService {
     }
 
     public List<Product> getAllProducts() {
-//        Iterable<Product> it=productRepo.findAll();
-//        ArrayList<Product> products=new ArrayList<>();
-//        it.forEach(products::add);
-//        return products;
-
-//        return StreamSupport.stream(productRepo.findAll().spliterator(), false)
-//                    .collect(Collectors.toList());
         return productRepo.findAll();
     }
 
@@ -46,13 +39,13 @@ public class ProductService {
         if (categorie == CategorieBautura.ALL) return getAllProducts();
         return getAllProducts().stream()
                 .filter(p -> p.getCategorie() == categorie)
-                .collect(Collectors.toList());
+                .toList();
     }
 
     public List<Product> filterByTip(TipBautura tip) {
         if (tip == TipBautura.ALL) return getAllProducts();
         return getAllProducts().stream()
                 .filter(p -> p.getTip() == tip)
-                .collect(Collectors.toList());
+                .toList();
     }
 }
