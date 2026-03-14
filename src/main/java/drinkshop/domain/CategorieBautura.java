5f0f2338c8f085905658222279a427e54e42af35
vladmutu <vladmutu05@gmail.com>
2026-03-14 20:08:45 +0200

    .

---
+++ src/main/java/drinkshop/domain/CategorieBautura.java
@@ -0,0 +1,16 @@
+package drinkshop.domain;
+
+public enum CategorieBautura {
+    CLASSIC_COFFEE,
+    MILK_COFFEE,
+    SPECIAL_COFFEE,
+    ICED_COFFEE,
+
+    TEA,
+    BUBBLE_TEA,
+
+    JUICE,
+    SMOOTHIE,
+
+    ALL
+}
